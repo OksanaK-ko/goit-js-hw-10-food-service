1f2e36cdfff61278d215b18c94fcf5a012c50303
Oksana Kochenko <oksanakochenkoo@gmail.com>
2020-11-09 12:57:36 +0300

    Вынесла обращение к ДОМ в отдельный модуль

---
+++ src/container.js
@@ -0,0 +1,6 @@
+
+export const menuContainer = document.querySelector('.js-menu');
+export const bodyTheme = document.querySelector('body');
+export const themeSwitcher = document.querySelector('#theme-switch-toggle');
+
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,46 @@
 import './styles.css';
-import './menu-cards';
-import './theme-change';
+import menuItemsTmpl from './templates/menu-items.hbs';
+import menuItems from './menu.json';
+import { menuContainer, bodyTheme, themeSwitcher } from './container';
+
+const menuCardsmarkup = createMenuCardsmarkup(menuItems);
+menuContainer.insertAdjacentHTML('beforeend', menuCardsmarkup);
+
+console.log(menuItems);
+
+function createMenuCardsmarkup(menuItems) {
+    return menuItemsTmpl(menuItems);
+}
+  
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+themeSwitcher.addEventListener('change', onInputChange);
+function onInputChange(evt) {
+    if (evt.target.checked) {
+        bodyTheme.classList.toggle(Theme.DARK);
+        localStorage.setItem('theme-mode', 'dark');
+    }
+    else {
+        bodyTheme.classList.add(Theme.LIGHT)
+        bodyTheme.classList.remove(Theme.DARK)
+        localStorage.setItem('theme-mode', 'light');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', themeCheck);
+function themeCheck() {
+    
+    if (localStorage.getItem('theme-mode') === 'dark') {
+        bodyTheme.classList.add(Theme.DARK);  
+        themeSwitcher.checked = true;
+    }
+    else {
+        bodyTheme.classList.add(Theme.LIGHT);    
+        themeSwitcher.checked = false;
+    }
+}
+
 
